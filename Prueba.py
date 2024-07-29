69851ba3bba8f744422a7aadfc97d9ca81f0480b
MatiasCatalanR <mcata010@contratistas.codelco.cl>
2024-07-29 14:45:30 -0400

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,23 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
+app = dash.Dash(__name__)
+
+# Datos para el gráfico
+data = px.data.iris()
+
+# Crear el gráfico utilizando Plotly Express
+fig = px.scatter(data, x="sepal_width", y="sepal_length", color="species")
+
+app.layout = html.Div(
+    children=[
+        html.H1("Prueba N°2 Migración desde Streamlit a Dash"),
+        html.P("TICA."),
+        dcc.Graph(figure=fig)  # Agregar el gráfico a la página utilizando el componente dcc.Graph
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
