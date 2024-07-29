78b44fc5115177f0f23abc2c7d52817c8330b6f2
MatiasCatalanR <mcata010@contratistas.codelco.cl>
2024-07-29 17:26:21 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,25 +1,31 @@
-import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import pandas as pd
 
+# Crear una instancia de la aplicación Dash
 app = dash.Dash(__name__)
-server = app.server
 
-# Datos para el gráfico
-data = px.data.iris()
+# Datos de ejemplo para el gráfico
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-# Crear el gráfico utilizando Plotly Express
-fig = px.scatter(data, x="sepal_width", y="sepal_length", color="species")
+# Crear un gráfico de barras
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(
-    children=[
-        html.H1("Prueba N°2 Migración desde Streamlit a Dash"),
-        html.P("TICA."),
-        dcc.Graph(figure=fig)  # Agregar el gráfico a la página utilizando el componente dcc.Graph
-    ]
-)
+# Definir el layout de la aplicación
+app.layout = html.Div(children=[
+    html.H1(children='Prueba TICA'),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
-if __name__ == "__main__":
+# Ejecutar la aplicación
+if __name__ == '__main__':
     app.run_server(debug=True)
